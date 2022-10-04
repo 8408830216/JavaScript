0c55a692daacf32b95bf0ef3e983f24500f479a6
8408830216 <prashantpatil8798@gmail.com>
2022-10-04 13:04:58 +0530

    First Program execution in JS
    First Program execution in JS

---
+++ demo.js
@@ -0,0 +1,9 @@
+
+console.log("Hello");
+console.log("My name is Prashant Patil");
+//This is Information
+console.info("Iam from Pune");
+//This is warning
+console.warn("Graduate Student");
+//This is error
+console.error("Iam not that bad guy");
---
+++ jsAssign.js
@@ -0,0 +1,10 @@
+
+console.log("Prashant Gulabrao Patil");
+console.log("Hobbies-Photography,Cooking,Travelling");
+console.info("Strength- 1)Creativity 2)Enthusiasm 3)Leadership");
+console.warn("Weakness- 1)Strong Willed 2)Impatience 3)Focusing On small Details");
+console.log(30 + 50);
+//Expected Outcome :80
+console.log(49 * 479);
+//Expected Outcome :23,471
+
