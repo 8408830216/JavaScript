2c3d330b74f99416d70b066e72a0579baef1bad7
8408830216 <prashantpatil8798@gmail.com>
2022-10-15 15:31:19 +0530

    Update 06_forLoop.js

--- 06_forLoop.js
+++ 06_forLoop.js
@@ -2,10 +2,12 @@
 for (var test = 1; test <= 10; test++) { // test = 1  2  3
     console.log(test); // 1 2  3
 }
+console.log("================================================================");
 // 0 2 4 6 8 10
 for (let index = 0; index <= 10; index=index+2) { // index++   index = index+2
     console.log(index);
 }
+console.log("================================================================");
 
 // WAP to print numbers from 50 to 40 by decrementing by 1
 for (let index = 50; index > 40; index--) { // 51  52  53 
@@ -13,7 +15,7 @@ for (let index = 50; index > 40; index--) { // 51  52  53
     
 }
 
-
+console.log("================================================================");
 // WAP to find first 15 odd numbers
 // intialization index=1 ; condition index<30;  update index= index+2 
 
@@ -21,7 +23,7 @@ for (let index = 1; index < 30; index=index+2) {
     console.log(index);
     
 }
-
+console.log("================================================================");
 
 for (let index = 1; index < 30; index++) {
     if(index%2!=0){
@@ -29,8 +31,8 @@ for (let index = 1; index < 30; index++) {
     }
     
 }
-
-//WAP to find first 43 even numbers
+console.log("================================================================");
+// WAP to find first 43 even numbers
 var countEven = 0; // 2
 for (let index = 0; index < 1000; index++) { // 1  2
     if(index%2==0){
@@ -38,6 +40,17 @@ for (let index = 0; index < 1000; index++) { // 1  2
         countEven = countEven +1;
         if (countEven==43) {
             break;
+        }
+    }
+}
+console.log("================================================================");
+ var oddNu=0;   
+for (let index = 10; index<500; index++) {
+    if (index%2!=0) {
+        console.log(index);
+        oddNu=oddNu+1;
+        if (oddNu==10) {
+    break;           
         }
     }
     
