15278315fd479b10f153b4b8d515fa398f88d6ae
8408830216 <prashantpatil8798@gmail.com>
2022-10-18 22:55:25 +0530

    18 oct update
    18 oct update

---
+++ 06_assignment03.js
@@ -0,0 +1,46 @@
+console.log("==============================1)======================================");
+let count=5;
+while (count<=15) {
+    console.log(count);
+    count++
+}
+
+console.log("==============================2)======================================");
+let numbers=50;
+do {
+    console.log(numbers);
+    numbers--
+} while (numbers>=40);
+
+console.log("==============================3)======================================");
+let oddNum=0;
+while (oddNum<=30) {
+    if (oddNum%2!=0) {
+        console.log(oddNum);
+    } oddNum++
+}
+console.log("==============================4)======================================");
+let evenNum=1;
+while (evenNum<=20) {
+    if (evenNum%2==0) {
+        console.log(evenNum);
+    } evenNum++
+}
+console.log("==============================5)======================================");
+let tableFive=5;
+while (tableFive<=50) {
+    console.log(tableFive);
+    tableFive=tableFive+5;
+}
+console.log("==============================6)======================================");
+let tableTen=10;
+while (tableTen<=100) {
+    console.log(tableTen);
+    tableTen=tableTen+10;
+}
+console.log("==============================7)======================================");
+let revTable=100;
+do {
+    console.log(revTable);
+    revTable=revTable-10;
+} while (revTable>=10);
